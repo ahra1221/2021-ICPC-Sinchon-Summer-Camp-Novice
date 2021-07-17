7b4262463cc9694d3f6e011e24364b493268521b
ahra <lizze1221@sookmyung.ac.kr>
2021-07-18 01:31:11 +0900

    [1일차]

---
+++ 1547.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int cnt, a, b;
+//	int ball = 1;
+//	cin >> cnt;
+//
+//	for (int i = 0; i < cnt; i++) {
+//		cin >> a >> b;
+//		if (a == ball) {
+//			ball = b;
+//		}
+//		else if (b == ball) {
+//			ball = a;
+//		}
+//	}
+//
+//	cout << ball << "\n";
+//
+//	return 0;
+//}
---
+++ 15552.cpp
@@ -0,0 +1,15 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	cin.tie(NULL);
+//	ios_base::sync_with_stdio(false);
+//
+//	int cnt, a, b;
+//	cin >> cnt;
+//	for (int i = 0; i < cnt; i++) {
+//		cin >> a >> b;
+//		cout << a + b << "\n";
+//	}
+//	return 0;
+//}
