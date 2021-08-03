e565f1ecf34f2ed75e87d347789eca40edde3f66
ahra Ko <79412889+ahra1221@users.noreply.github.com>
2021-08-03 17:54:51 +0900

    Update 1932.cpp

--- week5/1932.cpp
+++ week5/1932.cpp
@@ -18,11 +18,12 @@ int main() {
 	}
 	for (int i = 1; i < n; i++) {
 		for (int j = 0; j <= i; j++) {
-			if (j == 0) dp[i][j] = dp[i - 1][0] + dp[i][j];
-			else if (j == i) dp[i][j] = dp[i - 1][j - 1] + dp[i][j];
-			else dp[i][j] = max(dp[i - 1][j - 1] + dp[i][j], dp[i - 1][j] + dp[i][j]);
+			if (j == 0) dp[i][j] = dp[i - 1][0] + dp[i][j]; // 각 줄에 있는 첫번째 값
+			else if (j == i) dp[i][j] = dp[i - 1][j - 1] + dp[i][j]; // 각 줄에서 제일 마지막에 있는 값
+			else dp[i][j] = max(dp[i - 1][j - 1] + dp[i][j], dp[i - 1][j] + dp[i][j]); 
+			// 위의 두 경우가 아니라 가운데에 있는 원소라면 왼쪽 위의 값과 오른쪽 위의 값 중 최댓값을 구해서 더해준다.
 
-			ans = max(ans, dp[i][j]);
+			ans = max(ans, dp[i][j]); // 최댓값 계속 
 		}
 	}
 	cout << ans << "\n";
