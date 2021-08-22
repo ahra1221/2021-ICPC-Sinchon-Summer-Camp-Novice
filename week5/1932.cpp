864102641407249e216a035757dd57aebd54b3d5
ahra Ko <79412889+ahra1221@users.noreply.github.com>
2021-08-22 20:47:04 +0900

    Update 1932.cpp

--- week5/1932.cpp
+++ week5/1932.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+
 using namespace std;
 
 int main() {
@@ -8,22 +9,28 @@ int main() {
 
 	int n;
 	int ans = 0;
+	int dp[501][501] = {};
+
 	cin >> n;
-	int dp[501][501] = { 0, };
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j <= i; j++) {
 			cin >> dp[i][j];
 		}
 	}
+
+	if (n == 1) {
+		cout << dp[0][0]<<"\n";
+		return 0;
+	}
 	for (int i = 1; i < n; i++) {
 		for (int j = 0; j <= i; j++) {
-			if (j == 0) dp[i][j] = dp[i - 1][0] + dp[i][j]; // 각 줄에 있는 첫번째 값
-			else if (j == i) dp[i][j] = dp[i - 1][j - 1] + dp[i][j]; // 각 줄에서 제일 마지막에 있는 값
-			else dp[i][j] = max(dp[i - 1][j - 1] + dp[i][j], dp[i - 1][j] + dp[i][j]); 
-			// 위의 두 경우가 아니라 가운데에 있는 원소라면 왼쪽 위의 값과 오른쪽 위의 값 중 최댓값을 구해서 더해준다.
+			if (j == 0) dp[i][j] = dp[i - 1][j] + dp[i][j]; // 줄에서 첫 번째 값
+			else if (j == i) dp[i][j] = dp[i - 1][j - 1] + dp[i][j]; // 줄에서 마지막 값
+			else dp[i][j] = max(dp[i - 1][j - 1] + dp[i][j], dp[i - 1][j] + dp[i][j]);
+			// 가운데 있는 값들은 최댓값을 비교하여 넣어준다.
 
-			ans = max(ans, dp[i][j]); // 최댓값 계속 
+			ans = max(ans, dp[i][j]);
 		}
 	}
 	cout << ans << "\n";
